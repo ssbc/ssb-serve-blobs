47cafef5b34e34a6fdb45a7ac504226f81c1b33d
Andre Staltz <andre@staltz.com>
2021-09-28 14:50:40 +0300

    fix idToUrl when unbox is set

--- id-to-url.js
+++ id-to-url.js
@@ -4,7 +4,7 @@ module.exports = function idToUrl(blobId, params) {
   const port = (params && params.port) || DEFAULT_PORT;
   const blobRef = encodeURIComponent(blobId);
   const paramsStr = (params && params.unbox)
-    ? `?unbox=${encodeURIComponent(params.unbox.toString('base64'))}`
+    ? `?unbox=${encodeURIComponent(params.unbox.toString('base64'))}.boxs`
     : '';
 
   return `http://localhost:${port}/get/${blobRef}${paramsStr}`;
