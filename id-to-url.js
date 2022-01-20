0cfc3d9b968ff89a85e49bca04a07fca9badba55
Andre Staltz <andre@staltz.com>
2022-01-20 14:08:19 +0200

    id-to-url.js supports query params

--- id-to-url.js
+++ id-to-url.js
@@ -2,10 +2,13 @@ const DEFAULT_PORT = require('./port');
 
 module.exports = function idToUrl(blobId, params) {
   const port = (params && params.port) || DEFAULT_PORT;
-  const blobRef = encodeURIComponent(blobId);
-  const paramsStr = (params && params.unbox)
+  const [pureBlobId, query] = blobId.split('?');
+  const blobRef = encodeURIComponent(pureBlobId);
+  const paramsStr = query
+    ? '?' + query
+    : params && params.unbox
     ? `?unbox=${encodeURIComponent(params.unbox.toString('base64'))}.boxs`
     : '';
 
   return `http://localhost:${port}/get/${blobRef}${paramsStr}`;
-}
+};
