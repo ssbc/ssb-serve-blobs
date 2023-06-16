e0909e0d66efaf746e0e627ed9f408df94fa5a30
Andrew Chou <andrewchou@fastmail.com>
2023-06-16 12:44:50 -0400

    add option to specify port for url-to-id

--- url-to-id.js
+++ url-to-id.js
@@ -1,6 +1,8 @@
-const PORT = require('./port');
+const DEFAULT_PORT = require('./port');
 
-module.exports = function urlToId(url) {
-  const blobRef = url.split(`http://localhost:${PORT}/get/`)[1];
+module.exports = function urlToId(url, opts = { port: DEFAULT_PORT }) {
+  if (opts.port && typeof opts.port !== 'number')
+    throw new Error('port option must be a number');
+  const blobRef = url.split(`http://localhost:${opts.port}/get/`)[1];
   return decodeURIComponent(blobRef);
-}
+};
